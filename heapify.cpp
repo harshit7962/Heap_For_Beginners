66b872e23f79780b01b2325720df45a64766deb1
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-06-14 17:57:31 +0530

    Create heapify.cpp

---
+++ heapify.cpp
@@ -0,0 +1,12 @@
+//Given a heap, which might violate the property of min-heap at its node, we need to convert it to min-heap
+void minheapify(int i) {
+    int lt = left(i), rt = right(i);
+    smallest = i;
+    if(lt<size && arr[lt]<arr[smallest]) smallest = lt;
+    if(rt<size && arr[rt]<arr[smallest]) smallest = rt;
+        
+    if(smallest!=i) {
+      swap(arr[i], arr[smallest]);
+      minheapify(smallest);
+    }
+}
